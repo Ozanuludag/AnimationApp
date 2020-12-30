973e3b3086fcc3637688eabe7bbcf4fad9f8e380
Ozan Uludağ <ozanuludag@pop-os.localdomain>
2020-12-31 02:17:03 +0300

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState,useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  LayoutAnimation,
+  UIManager,
+  Platform,
+} from "react-native";
+import data from "./src/data.json";
 
 export default function App() {
+  useEffect(() => {
+    if (
+      Platform.OS === 'android' &&
+      UIManager.setLayoutAnimationEnabledExperimental
+    ) {
+      UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
+  });
+  const [id, setId] = useState();
+  const [show, setShow] = useState(false);
+  const combine = (id) => {
+    setId(id);
+    setShow(!show);
+  }
+  const renderComponent = ({ item }) => {
+    
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+
+    return (
+      <View style={styles.itemContainer}>
+        <TouchableOpacity onPress={() => combine(item.id)}>
+          <Text style={styles.itemTitle}>{item.title}</Text>
+          {id === item.id && show ? <Text>{item.description}</Text> : null}
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={data}
+        renderItem={renderComponent}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
 }
@@ -14,8 +54,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignContent: "center",
+    justifyContent: "center",
+    margin: 10,
+    marginTop: 50,
   },
+  itemContainer: {
+    flex: 1,
+    borderColor: "#b7b7a4",
+    borderWidth: 1,
+    padding: 10,
+    marginVertical:2,
+    borderRadius:15
+  },
+  itemTitle:{
+    fontSize: 24, 
+  }
 });
